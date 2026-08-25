bd464cc57abeb9169f600255143672dbd58f93a4
Vlad <koooornienko@gmail.com>
2026-08-25 22:12:43 +0500

    fix(i18n): add missing import for useTranslation in VideoTutorialModal.tsx

--- src/components/ui/VideoTutorialModal.tsx
+++ src/components/ui/VideoTutorialModal.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
+import { useTranslation } from "react-i18next";
 import { useTheme } from "../../context/ThemeContextInstance";
 import { fireConfetti } from "../../utils/confetti";
 
